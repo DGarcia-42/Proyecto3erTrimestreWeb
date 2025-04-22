e7dd74f1c3b8359a3db70bbf4ec9d8cb16e77bc0
DGarcia-42 <diiegogarsan@gmail.com>
2025-04-22 14:51:12 +0200

    about us completo

--- Web/dom.js
+++ Web/dom.js
@@ -1,1 +1,23 @@
+// Función para inicializar los desplegables de las preguntas frecuentes
+function initFAQAccordion() {
+    const faqItems = document.querySelectorAll('.faq__question');
+    
+    faqItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentElement;
+            
+            parent.classList.toggle('active');
+            const siblings = Array.from(parent.parentNode.children).filter(child => child !== parent);
+            siblings.forEach(sibling => {
+                sibling.classList.remove('active');
+            });
+        });
+    });
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    initFAQAccordion();
+    
+    
+});
